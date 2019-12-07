a4032ca3607810a0b7ce62abd3b4c09f367a1761
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2019-12-07 12:06:03 -0300

    Some lints warnings fixed

--- Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/utils/TouchListener.java
+++ Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/utils/TouchListener.java
@@ -1,9 +1,11 @@
 package ar.edu.uade.scrumgame.presentation.view.utils;
 
+import android.annotation.SuppressLint;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class TouchListener implements View.OnTouchListener {
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
