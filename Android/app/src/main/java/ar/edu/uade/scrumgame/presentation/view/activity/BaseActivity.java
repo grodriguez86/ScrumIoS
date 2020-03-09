64906ec883fd9d4a20c513b0a33a7b6e33c5cf10
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2020-03-08 21:47:19 -0300

    Gradient status bar implemented

--- Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/activity/BaseActivity.java
+++ Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/activity/BaseActivity.java
@@ -1,13 +1,21 @@
 package ar.edu.uade.scrumgame.presentation.view.activity;
 
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.view.Window;
+import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import javax.inject.Inject;
 
+import androidx.core.content.ContextCompat;
+import ar.edu.uade.scrumgame.R;
 import ar.edu.uade.scrumgame.presentation.ScrumApplication;
 import ar.edu.uade.scrumgame.presentation.di.components.ApplicationComponent;
 import ar.edu.uade.scrumgame.presentation.di.modules.ActivityModule;
@@ -22,6 +30,19 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getApplicationComponent().inject(this);
+        setStatusBarGradiant(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public static void setStatusBarGradiant(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            Drawable background = ContextCompat.getDrawable(activity,R.drawable.gradient_background_logo);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setNavigationBarColor(activity.getResources().getColor(android.R.color.transparent));
+            window.setBackgroundDrawable(background);
+        }
     }
 
     protected void addFragment(int containerViewId, Fragment fragment) {
