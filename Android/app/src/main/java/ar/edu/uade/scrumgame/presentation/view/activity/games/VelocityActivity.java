44ba5984c05ad8b1280cd49deb192f2528664872
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2020-03-30 21:41:29 -0300

    Velocity conclusion screens implemented

--- Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/activity/games/VelocityActivity.java
+++ Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/activity/games/VelocityActivity.java
@@ -1,54 +1,60 @@
 package ar.edu.uade.scrumgame.presentation.view.activity.games;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-import android.widget.EditText;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import ar.edu.uade.scrumgame.R;
 import ar.edu.uade.scrumgame.presentation.view.activity.BaseActivity;
-import ar.edu.uade.scrumgame.velocity.UnityPlayerActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import ar.edu.uade.scrumgame.presentation.view.fragment.games.VelocityConclusionGameFragment;
+import ar.edu.uade.scrumgame.presentation.view.fragment.games.VelocityResultsGameFragment;
+import ar.edu.uade.scrumgame.presentation.view.fragment.games.VelocityIntroGameFragment;
 
-public class VelocityActivity extends BaseActivity {
-    private static int ON_GAME_COMPLETED_CODE = 90;
-    @BindView(R.id.expectedScoreTv)
-    EditText expectedScore;
-    @BindView(R.id.playBtn)
-    Button playButton;
+public class VelocityActivity extends BaseActivity implements VelocityIntroGameFragment.OnVelocityGameFinishedListener, VelocityResultsGameFragment.OnContinueToConclusionListener, VelocityConclusionGameFragment.OnVelocityConclusionFinishedListener {
+    private static final int INDETERMINATE_PROGRESS_FEATURE_ID = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS | Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(INDETERMINATE_PROGRESS_FEATURE_ID);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setContentView(R.layout.activity_game_velocity);
-        ButterKnife.bind(this);
+        this.initializeFragment(savedInstanceState);
+    }
+
+    private void initializeFragment(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            Fragment fragment = VelocityIntroGameFragment.newInstance();
+            this.addFragment(fragment);
+        }
+    }
+
+    private void addFragment(Fragment fragment) {
+        final FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
+    }
+
+
+    @Override
+    public void onGameFinished(int expectedScore, int score) {
+        Fragment fragment = VelocityResultsGameFragment.newInstance(expectedScore, score);
+        this.addFragment(fragment);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ON_GAME_COMPLETED_CODE) {
-            Intent returnIntent = new Intent();
-            returnIntent.putExtra("currentScore", data.getIntExtra("currentScore", 0));
-            setResult(Activity.RESULT_OK, returnIntent);
-            finish();
-        }
+    public void continueToConclusion() {
+        Fragment fragment = VelocityConclusionGameFragment.newInstance();
+        this.addFragment(fragment);
     }
 
-    @OnClick(R.id.playBtn)
-    public void playGame() {
-        if (!expectedScore.getText().toString().isEmpty()) {
-            Intent intent = new Intent(this, UnityPlayerActivity.class);
-            intent.putExtra("expectedScore", Integer.valueOf(expectedScore.getText().toString()));
-            startActivityForResult(intent, ON_GAME_COMPLETED_CODE);
-        }
+    @Override
+    public void onVelocityGameEnded() {
+        Intent returnIntent = new Intent();
+        setResult(RESULT_OK, returnIntent);
+        finish();
     }
 }
---
+++ Android/app/src/main/java/ar/edu/uade/scrumgame/presentation/view/fragment/games/VelocityResultsGameFragment.java
@@ -0,0 +1,106 @@
+package ar.edu.uade.scrumgame.presentation.view.fragment.games;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.fragment.app.Fragment;
+import ar.edu.uade.scrumgame.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class VelocityResultsGameFragment extends Fragment {
+    public interface OnContinueToConclusionListener {
+        void continueToConclusion();
+    }
+
+    private static final String BUNDLE_EXTRA_PARAM_EXPECTED_SCORE = "ar.edu.uade.scrumgame.BUNDLE_EXTRA_PARAM_EXPECTED_SCORE";
+    private static final String BUNDLE_EXTRA_PARAM_SCORE = "ar.edu.uade.scrumgame.BUNDLE_EXTRA_PARAM_SCORE";
+    @BindView(R.id.expectedScoreTv)
+    AppCompatTextView expectedScoreTv;
+    @BindView(R.id.scoreTv)
+    AppCompatTextView scoreTv;
+    @BindView(R.id.resultsDescriptionTv)
+    AppCompatTextView resultsDescriptionTv;
+    @BindView(R.id.continueBtn)
+    Button continueButton;
+    private int score;
+    private int expectedScore;
+    private OnContinueToConclusionListener onContinueToConclusionListener;
+
+    public static VelocityResultsGameFragment newInstance(int expectedScore, int score) {
+        Bundle bundle = new Bundle();
+        bundle.putInt(BUNDLE_EXTRA_PARAM_EXPECTED_SCORE, expectedScore);
+        bundle.putInt(BUNDLE_EXTRA_PARAM_SCORE, score);
+        VelocityResultsGameFragment fragment = new VelocityResultsGameFragment();
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnContinueToConclusionListener) {
+            this.onContinueToConclusionListener = (OnContinueToConclusionListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.onContinueToConclusionListener = null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getGamesData();
+    }
+
+    private void getGamesData() {
+        if (this.getArguments() != null) {
+            this.score = this.getArguments().getInt(BUNDLE_EXTRA_PARAM_SCORE, 0);
+            this.expectedScore = this.getArguments().getInt(BUNDLE_EXTRA_PARAM_EXPECTED_SCORE, 0);
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.expectedScoreTv.setText(String.valueOf(this.expectedScore));
+        this.scoreTv.setText(String.valueOf(this.score));
+        this.resultsDescriptionTv.setText(getString(this.getScoreDescription()));
+    }
+
+    private int getScoreDescription() {
+        if (this.score > this.expectedScore) {
+            return R.string.velocity_game_lower_estimation;
+        }
+        if (this.score < this.expectedScore) {
+            return R.string.velocity_game_higher_estimation;
+        }
+        return R.string.velocity_game_exact_estimation;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.fragment_game_velocity_results, container, false);
+        ButterKnife.bind(this, fragmentView);
+        return fragmentView;
+    }
+
+    @OnClick(R.id.continueBtn)
+    public void onContinueButtonClicked() {
+        if (this.onContinueToConclusionListener != null) {
+            this.onContinueToConclusionListener.continueToConclusion();
+        }
+    }
+
+}
