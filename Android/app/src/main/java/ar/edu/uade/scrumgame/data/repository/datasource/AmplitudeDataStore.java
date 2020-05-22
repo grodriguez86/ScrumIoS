0f3932153aea50d3bf026f2b78141f92cd004422
Ana Martinez Saucedo <anmartinez@uade.edu.ar>
2020-05-21 22:36:11 -0300

    Log event error handling

--- Android/app/src/main/java/ar/edu/uade/scrumgame/data/repository/datasource/AmplitudeDataStore.java
+++ Android/app/src/main/java/ar/edu/uade/scrumgame/data/repository/datasource/AmplitudeDataStore.java
@@ -20,7 +20,11 @@ class AmplitudeDataStore implements LogEventDataStore {
     @Override
     public Observable<Void> initialize() {
         return Observable.create(emitter -> {
-            Amplitude.getInstance().initialize(this.context, BuildConfig.AMPLITUDE_API_KEY).enableForegroundTracking((Application) this.context);
+            try {
+                Amplitude.getInstance().initialize(this.context, BuildConfig.AMPLITUDE_API_KEY).enableForegroundTracking((Application) this.context);
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
             emitter.onComplete();
         });
     }
@@ -28,7 +32,11 @@ class AmplitudeDataStore implements LogEventDataStore {
     @Override
     public Observable<Void> logEvent(String eventName, JSONObject eventParams) {
         return Observable.create(emitter -> {
-            Amplitude.getInstance().logEvent(eventName, eventParams);
+            try {
+                Amplitude.getInstance().logEvent(eventName, eventParams);
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
             emitter.onComplete();
         });
     }
